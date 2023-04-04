9de8959c0fdac86ee2b59438dd072dcc9d1e30b4
Canthar90 <d.cieslak90@gmail.com>
2023-04-04 15:17:07 +0200

    Testing some basic functionality

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-$("h1").css("color", "red");
 
+
+$("h1").addClass("big-title margin-50");
+
+$("button").html("<em>Heyyy</em>");
+
+$("h1").click(function(){
+    $("h1").css("color", "purple")
+});
+
+$("input").keydown(function(event) {
+    console.log(event.key)
+});
+
+$(document).keydown(function(event){
+    $("h1").text(event.key)
+})
